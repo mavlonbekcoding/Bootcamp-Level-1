7c66483c6e68f144e69fd65443228b9532b5bd04
Mavlonbek Isroilov <mavlonbekcoding@gmail.com>
2023-08-13 15:28:10 +0500

    efqw

---
+++ N21-HT1/CustomList.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace N21_HT1
+{
+    class CustomList<T>
+    {
+        private List<T> internalList;
+
+        public CustomList()
+        {
+            internalList = new List<T>();
+        }
+
+        public CustomList(int capacity)
+        {
+            internalList = new List<T>(capacity);
+        }
+
+        public void Add(T item)
+        {
+            internalList.Add(item);
+        }
+
+        public void AddRange(IEnumerable<T> items)
+        {
+            internalList.AddRange(items);
+        }
+
+        public bool Contains(T item)
+        {
+            return internalList.Contains(item);
+        }
+
+        public void CopyTo(T[] array)
+        {
+            internalList.CopyTo(array);
+        }
+
+        public int IndexOf(T item)
+        {
+            return internalList.IndexOf(item);
+        }
+
+        public void Insert(int index, T item)
+        {
+            internalList.Insert(index, item);
+        }
+
+        public void InsertRange(int index, IEnumerable<T> items)
+        {
+            internalList.InsertRange(index, items);
+        }
+
+        public bool Remove(T item)
+        {
+            return internalList.Remove(item);
+        }
+
+        public bool RemoveAt(int index)
+        {
+            if (index >= 0 && index < internalList.Count)
+            {
+                internalList.RemoveAt(index);
+                return true;
+            }
+            return false;
+        }
+
+        public T[] ToArray()
+        {
+            return internalList.ToArray();
+        }
+    }
+}
---
+++ N21-HT1/Program.cs
@@ -0,0 +1,19 @@
+﻿using N21_HT1;
+
+CustomList<int> myList = new CustomList<int>();
+myList.Add(5);
+myList.AddRange(new List<int> { 10, 15, 20 });
+Console.WriteLine(myList.Contains(10)); // Output: True
+Console.WriteLine(myList.IndexOf(15));  // Output: 2
+
+int[] array = new int[4];
+myList.CopyTo(array);
+Console.WriteLine(string.Join(", ", array)); // Output: 5, 10, 15, 20
+
+myList.Insert(1, 25);
+myList.InsertRange(2, new List<int> { 30, 35 });
+Console.WriteLine(string.Join(", ", myList.ToArray())); // Output: 5, 25, 30, 35, 10, 15, 20
+
+myList.Remove(25);
+myList.RemoveAt(3);
+Console.WriteLine(string.Join(", ", myList.ToArray())); // Output: 5, 30, 35, 15, 20
