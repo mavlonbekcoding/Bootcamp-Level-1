a9b75ca81c8d271da8fceeed38decd18f5f9d45e
Mavlonbek Isroilov <mavlonbekcoding@gmail.com>
2023-08-13 15:21:14 +0500

    qwer

---
+++ HT-20-2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        List<int> listA = new List<int> { 1, 2, 3, 4, 5 };
+        List<int> listB = new List<int> { 3, 4, 5, 6, 7 };
+
+        var result = listA.Except(listB).ToList();
+
+        foreach (var item in result)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
---
+++ N23-HT1/Product.cs
@@ -0,0 +1,12 @@
+﻿public class Product
+{
+    public string Name { get; set; }
+    public int Stars { get; set; }
+    public int Inventory { get; set; }
+    public Product(string name, int stars,int invertory) 
+    { 
+        this.Name = name;
+        this.Stars = stars;
+        this.Inventory = Inventory;
+    }
+}
