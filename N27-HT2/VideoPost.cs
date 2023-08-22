93da5c34cf371cc03053084f36a8590e44b992e7
Mavlonbek Isroilov <mavlonbekcoding@gmail.com>
2023-08-22 20:13:13 +0500

    ok no problem

---
+++ N27-HT2/VideoPost.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace N27_HT2
+{
+    public class VideoPost
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }   
+        public string Description { get; set; }
+        public int Likes { get; set; }
+        public int Dislikes { get; set; }
+        public Topics topic { get; set; }
+    }
+}
